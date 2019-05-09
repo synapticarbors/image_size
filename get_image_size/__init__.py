2c4f4fcbdedc1b38b996bcc01c41a467a38c8113
Jacob Tomlinson <jacob.tomlinson@informaticslab.co.uk>
2019-05-09 11:44:44 +0100

    Add versioneer

--- get_image_size/__init__.py
+++ get_image_size/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 from __future__ import print_function
 """
 
